daaabf80bfb58faef48fdf2ae65bbc57753c2a05
Maicius <maicius@outlook.com>
2019-07-31 16:07:13 +0800

    按brand_id 分

--- src/clusterData.py
+++ src/clusterData.py
@@ -5,6 +5,7 @@ import sys
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(os.path.split(rootPath)[0])
+import json
 
 class clusterData(object):
     pre = "../raw_data/ECommAI_ubp_round1_"
@@ -12,7 +13,7 @@ class clusterData(object):
     real_pre = "../mid_data_random/"
     res_pre = "../result_data/"
 
-    def __init__(self, small = True, sup = 0.3):
+    def __init__(self, small = True, sup = 0.1):
         # 置信度
         self.support_degree = sup
         self.small = small
@@ -24,25 +25,43 @@ class clusterData(object):
         self.item_cate_df = self.load_item_feature()
         self.train = self.load_train()
         item_cate_list = self.item_cate_df.values
-        self.item_cate_dict = {item[0]: item[1] for item in item_cate_list}
-        self.cate_item_dict = {item[1]: item[0] for item in item_cate_list}
+        # self.item_cate_dict = {item[0]: item[1] for item in item_cate_list}
+        # self.cate_item_dict = {item[1]: item[0] for item in item_cate_list}
         self.item_brand_dict = {item[0]: item[2] for item in item_cate_list}
         # self.train['cate_id'] = self.train['item_id'].apply(lambda x: self.replace_item_id(x))
         self.train['cate_id'] = self.train['item_id'].apply(lambda x: self.replace_item_id_with_brand(x))
         self.train['num'] = 1
         self.train.drop(index=self.train[self.train['cate_id'] == -1].index, inplace=True)
+        # 每个用户对应的品牌，去重
         self.train = self.train.groupby(by=['user_id', 'cate_id'])['num'].sum().reset_index()
         self.train['num'] = 1
-        self.total_user_num = len(set(self.train['user_id']))
+        self.user_set = set(self.train['user_id'])
+        self.total_user_num = len(self.user_set)
+        user_dict = self.cal_brand_id_with_user(df=self.train, user_set=self.user_set, item_cate_df=self.item_cate_df)
+        # 每个品牌在不同的用户中出现的次数
         self.item_num = self.train.groupby(by=['cate_id'])['num'].sum().reset_index()
         support_num = self.support_degree * self.total_user_num
         base_frequent_item_list = self.item_num[self.item_num['num'] > support_num]['cate_id'].values
         self.all_frequent_item_list = []
+        print(base_frequent_item_list)
         self.cal_frequent_items_iter(base_frequent_item_list, support_num)
         print("频繁项：", self.all_frequent_item_list)
         self.clean_frq_item_set()
         print("清洗后:", self.all_frequent_item_list)
+
         pass
+
+    def cal_brand_id_with_user(self, df, user_set, item_cate_df):
+        user_dict = {}
+        for user in user_set:
+            cate_list = df.loc[df.user_id == user, 'cate_id'].values
+            for cate in cate_list:
+                item_list = item_cate_df.loc[item_cate_df.brand_id == cate, 'item_id'].values
+                item_list = list(map(int, item_list))
+                user_dict[int(user)] = item_list
+        with open(self.mid_pre + "user_brand_item_dict.json", 'w', encoding='utf-8') as w:
+            json.dump(user_dict, w)
+        return user_dict
 
     def clean_frq_item_set(self):
         for i, item in enumerate(self.all_frequent_item_list):
@@ -61,6 +80,7 @@ class clusterData(object):
         for i in range(2, 4):
             print("组合数：", i)
             frequent_items = combinations(frequent_item_list, i)
+            find_combine = False
             for group in frequent_items:
                 group = set(group)
                 is_waste = False
@@ -79,8 +99,11 @@ class clusterData(object):
                             item_set = item_set.intersection(temp_set)
                     if len(item_set) > support_num:
                         self.all_frequent_item_list.append(group)
+                        find_combine = True
                     else:
                         waste_group.append(group)
+            if not find_combine:
+                break
 
 
     def replace_item_id(self, x):
